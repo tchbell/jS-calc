2ebab95f8b08b97e4c1fc89696ae18bc59f32444
Ted Bell <tchbell@gmail.com>
2017-12-27 15:45:54 -0500

    Basic multiplication works

--- script.js
+++ script.js
@@ -8,17 +8,75 @@ const view = {
       viewScreen.innerHTML += show;
     });
   },
+  //have a handler that sets what each button does with event delegation
   btnHandle: function(){
+    let mathType = {"type": undefined};
+    let mathHoldOne = {"num": 0, "set": false};
+    let mathHoldTwo = 0;
     let btnHandler = document.querySelector('.js-container');
+
     btnHandler.addEventListener('click', function(event){
       let btn = event.target;
-
+      let screenValue = document.querySelector('.js-view');
       switch(btn.className){
+        //clears whats in the view window
         case('cell clear'):
-        document.querySelector('.js-view').innerHTML = '';
-        console.log("ok");
-        break;
+          screenValue.innerHTML = '';
+          mathHoldOne.num = 0;
+          mathHoldOne.set = false;
+          mathHoldTwo = 0;
+          mathType.type = undefined;
+          break;
+        case('cell math multiply'):
+          if(mathHoldOne.set){
+            mathHoldTwo = parseInt(screenValue.innerHTML);
+            screenValue.innerHTML = '';
+          }else{
+            mathHoldOne.num = parseInt(screenValue.innerHTML);
+            mathHoldOne.set = true;
+            screenValue.innerHTML = '';
+            mathType.type = "mulitply";
+          }
+          break;
+        case('cell math divide'):
+          mathHoldOne= parseInt(screenValue.innerHTML);
+          screenValue.innerHTML = '';
+          mathType.type = "divide";
+          break;
+        case('cell math add'):
+          mathHoldOne = parseInt(screenValue.innerHTML);
+          screenValue.innerHTML = '';
+          mathType.type = "add";
+          break;
+        case('cell math subtract'):
+          mathHoldOne = parseInt(screenValue.innerHTML);
+          screenValue.innerHTML = '';
+          mathType.type = "subract";
+          break;
+        case('cell equal'):
+        mathHoldTwo = parseInt(screenValue.innerHTML);
+          if(mathType.type === "add"){
+            screenValue.innerHTML = (mathHoldOne.num + mathHoldTwo);
+            mathHoldOne.num = (mathHoldOne.num + mathHoldTwo);
+            mathHoldTwo = 0;
+          }else if(mathType.type === "subract"){
+            screenValue.innerHTML = (mathHoldOne.num - mathHoldTwo);
+            mathHoldOne.num = (mathHoldOne.num - mathHoldTwo);
+            mathHoldTwo = 0;
+          }
+          else if(mathType.type === "mulitply"){
+            screenValue.innerHTML = (mathHoldOne.num * mathHoldTwo);
+            mathHoldOne.num = (mathHoldOne.num * mathHoldTwo);
+            mathHoldTwo = 0;
+          }else if(mathType.type === "divide"){
+            screenValue.innerHTML = (mathHoldOne.num / mathHoldTwo);
+            mathHoldOne.num = (mathHoldOne.num / mathHoldTwo);
+            mathHoldTwo = 0;
+          }
+          break;
       }
+
+      console.log(mathHoldOne, mathHoldTwo, mathType.type);
     })
   }
 
