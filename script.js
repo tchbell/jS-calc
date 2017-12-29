5f37c873adae8c20ab728fd2041d72e61cbbe8e1
Ted Bell <tchbell@gmail.com>
2017-12-29 15:37:14 -0500

    added ability to chain mulitply math problems together. Still need to fix equal button issue

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const view = {
+  //Updates view when buttons are clicked
   updateView: function(){
     let viewScreen = document.getElementsByClassName('js-view')[0];
+    let miniView = document.getElementsByClassName('mini-view')[0];
     viewScreen.innerHTML = '';
+    miniView.innerHTML = '';
     const jsContainer = document.getElementsByClassName('js-container')[0];
     jsContainer.addEventListener('click', function(e){
       let show = e.target.innerHTML;
@@ -12,70 +15,154 @@ const view = {
   btnHandle: function(){
     let mathType = {"type": undefined};
     let mathHoldOne = {"num": 0, "set": false};
-    let mathHoldTwo = 0;
+    let mathHoldTwo = {"num": 0, "set": false};
     let btnHandler = document.querySelector('.js-container');
 
     btnHandler.addEventListener('click', function(event){
       let btn = event.target;
       let screenValue = document.querySelector('.js-view');
+      let miniView = document.querySelector('.mini-view');
       switch(btn.className){
         //clears whats in the view window
         case('cell clear'):
           screenValue.innerHTML = '';
+          miniView.innerHTML = '';
           mathHoldOne.num = 0;
           mathHoldOne.set = false;
-          mathHoldTwo = 0;
+          mathHoldTwo.num  = 0;
+          mathHoldTwo.set = false;
           mathType.type = undefined;
           break;
         case('cell math multiply'):
+        //assigns mathHoldTwo.num if mathHoldOne.set is true and blanks the screenValue
           if(mathHoldOne.set){
-            mathHoldTwo = parseInt(screenValue.innerHTML);
+            mathHoldTwo.num = parseInt(screenValue.innerHTML);
+            mathHoldTwo.set = true;
             screenValue.innerHTML = '';
+            //if mathHoldOne.set is false it assigns mathHoldOne.num and sets the set property to true
+            //also sets mathType.type to multiply
           }else{
             mathHoldOne.num = parseInt(screenValue.innerHTML);
             mathHoldOne.set = true;
             screenValue.innerHTML = '';
             mathType.type = "mulitply";
           }
+          if(mathHoldOne.set && mathHoldTwo.set){
+            //if both numbers are set cycle through calcFunc to find which mathType.type matches
+            //and execute that function with the two values
+            for(let name in calcFunc){
+              if(mathType.type === name){
+                miniView.innerHTML = calcFunc[name](mathHoldOne.num, mathHoldTwo.num);
+                mathHoldOne.num = calcFunc[name](mathHoldOne.num, mathHoldTwo.num);
+              }
+            }
+            mathHoldTwo.num = 0;
+            mathHoldTwo.set = false;
+            mathType.type = 'multiply';
+          }
           break;
         case('cell math divide'):
-          mathHoldOne= parseInt(screenValue.innerHTML);
-          screenValue.innerHTML = '';
-          mathType.type = "divide";
+          if(mathHoldOne.set){
+            mathHoldTwo.num = parseInt(screenValue.innerHTML);
+            mathHoldTwo.set = true;
+            screenValue.innerHTML = '';
+          }else{
+            mathHoldOne.num = parseInt(screenValue.innerHTML);
+            mathHoldOne.set = true;
+            screenValue.innerHTML = '';
+            mathType.type = "divide";
+          }
+          if(mathHoldOne.set && mathHoldTwo.set){
+            for(let name in calcFunc){
+              if(mathType.type === name){
+                miniView.innerHTML = calcFunc[name](mathHoldOne.num, mathHoldTwo.num);
+                mathHoldOne.num = calcFunc[name](mathHoldOne.num, mathHoldTwo.num);
+              }
+            }
+            mathHoldTwo.num = 0;
+            mathHoldTwo.set = false;
+            mathType.type = 'divide';
+          }
           break;
         case('cell math add'):
-          mathHoldOne = parseInt(screenValue.innerHTML);
-          screenValue.innerHTML = '';
-          mathType.type = "add";
+          if(mathHoldOne.set){
+            mathHoldTwo.num = parseInt(screenValue.innerHTML);
+            mathHoldTwo.set = true;
+            screenValue.innerHTML = '';
+          }else{
+            mathHoldOne.num = parseInt(screenValue.innerHTML);
+            mathHoldOne.set = true;
+            screenValue.innerHTML = '';
+            mathType.type = "add";
+          }
+          if(mathHoldOne.set && mathHoldTwo.set){
+            for(let name in calcFunc){
+              if(mathType.type === name){
+                miniView.innerHTML = calcFunc[name](mathHoldOne.num, mathHoldTwo.num);
+                mathHoldOne.num = calcFunc[name](mathHoldOne.num, mathHoldTwo.num);
+              }
+            };
+            mathHoldTwo.num = 0;
+            mathHoldTwo.set = false;
+            mathType.type = 'add';
+          }
           break;
         case('cell math subtract'):
-          mathHoldOne = parseInt(screenValue.innerHTML);
-          screenValue.innerHTML = '';
-          mathType.type = "subract";
+          if(mathHoldOne.set){
+            mathHoldTwo.num = parseInt(screenValue.innerHTML);
+            mathHoldTwo.set = true;
+            screenValue.innerHTML = '';
+          }else{
+            mathHoldOne.num = parseInt(screenValue.innerHTML);
+            mathHoldOne.set = true;
+            screenValue.innerHTML = '';
+            mathType.type = "subract";
+          }
+          if(mathHoldOne.set && mathHoldTwo.set){
+            for(let name in calcFunc){
+              if(mathType.type === name){
+                miniView.innerHTML = calcFunc[name](mathHoldOne.num, mathHoldTwo.num);
+                mathHoldOne.num = calcFunc[name](mathHoldOne.num, mathHoldTwo.num);
+              }
+            };
+            mathHoldTwo.num = 0;
+            mathHoldTwo.set = false;
+            mathType.type = 'subtract';
+          }
           break;
         case('cell equal'):
-        mathHoldTwo = parseInt(screenValue.innerHTML);
+        mathHoldTwo.num = parseInt(screenValue.innerHTML);
           if(mathType.type === "add"){
-            screenValue.innerHTML = (mathHoldOne.num + mathHoldTwo);
-            mathHoldOne.num = (mathHoldOne.num + mathHoldTwo);
-            mathHoldTwo = 0;
+            screenValue.innerHTML = calcFunc.add(mathHoldOne.num, mathHoldTwo.num);
+            miniView.innerHTML = calcFunc.add(mathHoldOne.num, mathHoldTwo.num);
+            mathHoldTwo.num = 0;
+            mathHoldOne.num = 0;
+            mathHoldOne.set = false;
           }else if(mathType.type === "subract"){
-            screenValue.innerHTML = (mathHoldOne.num - mathHoldTwo);
-            mathHoldOne.num = (mathHoldOne.num - mathHoldTwo);
-            mathHoldTwo = 0;
+            screenValue.innerHTML = calcFunc.subtract(mathHoldOne.num, mathHoldTwo.num);
+            miniView.innerHTML = calcFunc.subtract(mathHoldOne.num, mathHoldTwo.num);
+            mathHoldOne.num = (mathHoldOne.num - mathHoldTwo.num);
+            mathHoldTwo.num = 0;
+            mathHoldOne.num = 0;
+            mathHoldOne.set = false;
           }
           else if(mathType.type === "mulitply"){
-            screenValue.innerHTML = (mathHoldOne.num * mathHoldTwo);
-            mathHoldOne.num = (mathHoldOne.num * mathHoldTwo);
-            mathHoldTwo = 0;
+            screenValue.innerHTML = calcFunc.multiply(mathHoldOne.num, mathHoldTwo.num);
+            miniView.innerHTML = calcFunc.multiply(mathHoldOne.num, mathHoldTwo.num);
+            mathHoldOne.num = (mathHoldOne.num * mathHoldTwo.num);
+            mathHoldTwo.num = 0;
+            mathHoldOne.num = 0;
+            mathHoldOne.set = false;
           }else if(mathType.type === "divide"){
-            screenValue.innerHTML = (mathHoldOne.num / mathHoldTwo);
-            mathHoldOne.num = (mathHoldOne.num / mathHoldTwo);
-            mathHoldTwo = 0;
+            screenValue.innerHTML = calcFunc.divide(mathHoldOne.num, mathHoldTwo.num);
+            miniView.innerHTML = calcFunc.divide(mathHoldOne.num, mathHoldTwo.num);
+            mathHoldOne.num = (mathHoldOne.num / mathHoldTwo.num);
+            mathHoldTwo.num = 0;
+            mathHoldOne.num = 0;
+            mathHoldOne.set = false;
           }
           break;
       }
-
       console.log(mathHoldOne, mathHoldTwo, mathType.type);
     })
   }
@@ -84,16 +171,18 @@ const view = {
 view.updateView();
 view.btnHandle();
 const calcFunc = {
-  add: function(){
-
+  add: function(x, y){
+    return x + y;
   },
-  subtract: function(){
-
+  subtract: function(x,y){
+    return x-y;
   },
-  multiply: function(){
-
+  multiply: function(x, y){
+    return x * y;
   },
-
+  divide: function(x,y){
+    return x/y;
+  },
   clear: function(){
     let view = document.querySelector('js-view');
     view.innerHTML = '';
